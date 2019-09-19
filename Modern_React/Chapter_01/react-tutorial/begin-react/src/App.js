b7816a60e1f659ce1e139ee75d4b69db882659de
ksage <ksage@zinnaworks.com>
2019-09-19 13:55:30 +0900

    2019.09.18 벨로퍼트와 함께 하는 모던 리액트 : Chapter01 - 12

--- Modern_React/Chapter_01/react-tutorial/begin-react/src/App.js
+++ Modern_React/Chapter_01/react-tutorial/begin-react/src/App.js
@@ -1,9 +1,71 @@
-import React from 'react';
+// useRef - setTimeout, setInterval의 id, 외부라이브러리 인스턴스, scroll 위치 등에 사용
+// useRef를 사용할 경우 값이 바뀌어도 컴포넌트가 리렌더링 되지 않는다.
+
+import React, { useRef, useState } from 'react';
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const {username, email} = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
+  };
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'ksage',
+      email: 'ksage@ruu.kr'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@ruu.kr'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@ruu.kr'
+    }
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+
+    };
+
+    // setUsers([...users, user]);
+    setUsers(users.concat(user));
+    setInputs({
+      username: '',
+      email: ''
+    });
+    console.log(nextId.current);
+    nextId.current += 1;
+  };
+
   return (
-    <UserList />
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users}/>
+    </>
   );
 }
 
--- Modern_React/Chapter_01/react-tutorial/begin-react/src/UserList.js
+++ Modern_React/Chapter_01/react-tutorial/begin-react/src/UserList.js
@@ -10,50 +10,14 @@ function User({ user }) {
   )
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: 'ksage',
-      email: 'ksage@ruu.kr'
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@ruu.kr'
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@ruu.kr'
-    }
-  ];
-
+function UserList({users}) {
   return (
     <div>
-      {/*<div>*/}
-      {/*  <b>{users[0]}.username}</b> <span>({user[0].email})</span>*/}
-      {/*</div>*/}
-      {/*<div>*/}
-      {/*  <b>{users[1]}.username}</b> <span>({user[1].email})</span>*/}
-      {/*</div>*/}
-      {/*<div>*/}
-      {/*  <b>{users[2]}.username}</b> <span>({user[2].email})</span>*/}
-      {/*</div>*/}
-
-      {/*<User user={users[0]}/>*/}
-      {/*<User user={users[1]}/>*/}
-      {/*<User user={users[2]}/>*/}
-
       {
-        // 배열의 경우 key를 넘겨주지 않으면 계속 새로 렌더링함
         users.map(
-          (user, index)=> (
+          (user, index) => (
             <User user={user} key={index} />
           )
-          // (user) => (
-          //   <User user={user} key={user.id} />
-          // )
         )
       }
     </div>
