715901cb0f7f5b651d19dd781143129069fcb8c9
KangSage <ksage@knou.ac.kr>
2020-01-05 15:42:46 +0900

    Managing Asynchronous Action States Using Contexts

--- Modern_React/Chapter_04/api-integrate/src/Users.js
+++ Modern_React/Chapter_04/api-integrate/src/Users.js
@@ -1,43 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
+import User from './User';
+import { useUsersDispatch, getUsers, useUsersState } from './UsersContext';
+
+// async function getUsers() {
+//   const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+//   return res.data;
+// }
 
 function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [userId, setUserId] = useState(null);
+  // const { data: users, error, isLoading, reload, run } = useAsync({
+  //   deferFn: getUsers
+  // });
+  const state = useUsersState();
+  const dispatch = useUsersDispatch();
 
-  const fetchUsers = async () => {
-    try {
-      setUsers(null);
-      setError(null);
-      setLoading(true);
-      const res = await axios.get('https://jsonplaceholder.typicode.com/users');
-      setUsers(res.data);
-    } catch (e) {
-      setError(e);
-      console.log(e.response);
-    }
-    setLoading(false);
+  const { loading, data: users, error } = state.users;
+
+  const fetchData = () => {
+    getUsers(dispatch);
   };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
 
   if (loading) return <div>로딩중...</div>;
   if (error) return <div>에러가 발생하였습니다.</div>;
-  if (!users) return null;
-
+  if (!users) return <button onClick={fetchData}>불러오기</button>;
   return (
     <>
       <ul>
         {users.map(user => (
-          <li key={user.id}>
+          <li key={user.id} onClick={() => setUserId(user.id)}>
             {user.username} ({user.name})
           </li>
         ))}
       </ul>
-      <button onClick={fetchUsers}>다시 불러오기</button>
+      <button onClick={fetchData}>다시 불러오기</button>
+      {userId && <User id={userId} />}
     </>
   );
 }
